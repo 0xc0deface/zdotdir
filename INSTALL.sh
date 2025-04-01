cea4cbe62fbd4fd2cf6e49a9414a2468fcba40e2
matthew.fletcher <0xc0deface@gmail.com>
2025-04-01 10:34:08 +1000

    Enhance INSTALL.sh to support dynamic binary directory setup and version updates from GitHub

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,38 @@
+if [ -d "$HOME/bin" ]; then
+	export BIN_DIR="$HOME/bin"
+elif [ -d "$HOME/.local/bin" ]; then
+	export BIN_DIR="$HOME/.local/bin"
+else
+	mkdir -p "$HOME/bin"
+	export BIN_DIR="$HOME/bin"
+fi
+
 touch ~/.zshenv
-grep -qxF '"ZDOTDIR=$HOME/zdotdir"' ~/.zshenv || echo 'ZDOTDIR="$HOME/zdotdir"\n. $ZDOTDIR/.zshenv' >> ~/.zshenv
+grep -qxF 'ZDOTDIR=$HOME/zdotdir' ~/.zshenv || echo 'ZDOTDIR=$HOME/zdotdir'\\n'$ZDOTDIR/.zshenv' >> ~/.zshenv
 
-fzf_source=https://github.com/junegunn/fzf/releases/download/v0.60.3/fzf-0.60.3-linux_amd64.tar.gz
-wget -qO- $fzf_source | tar xvzfO - > ~/bin/fzf
-chmod +x ~/bin/fzf
+function update_from_github()
+{
+	# $1 = binary name
+	# $2 = github repo releases url
+	# $3 = download file name
+	current_version=$($BIN_DIR/$1 --version 2>/dev/null | grep -E "[0-9]+\.[0-9]+\.[0-9]+" | sed -E 's/.*([0-9]+\.[0-9]+\.[0-9]+).*/\1/' || echo "none")
+	latest_version=$(wget -qO- $2/latest | grep -Eo --color "/v[0-9]+\.[0-9]+\.[0-9]+" | sed -E "s/\/v(.*)/\1/" | head -n 1)
 
-eza_source=https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz
-wget -qO- $eza_source | tar xvzfO - > ~/bin/eza
-chmod +x ~/bin/eza
+	if [ "$current_version" != "$latest_version" ]; then
+		echo "Updating $1: current version ($current_version), latest version ($latest_version)"
+		download_file=$(echo $latest_version | sed -E "s/(.*)/$3/")
+		source=$2/download/v$latest_version/$download_file
+		echo $source
+		wget -qO- $source | tar xvzfO - > $BIN_DIR/$1
+		chmod +x $BIN_DIR/$1
+		echo "$1 updated to version $latest_version"
+	else
+		echo "$1 is already up-to-date (version $current_version)"
+	fi
+}
+
+update_from_github fzf https://github.com/junegunn/fzf/releases 'fzf-\1-linux_amd64.tar.gz'
+update_from_github eza https://github.com/eza-community/eza/releases eza_x86_64-unknown-linux-gnu.tar.gz
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
